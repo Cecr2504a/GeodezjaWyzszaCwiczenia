da0e6991772ea3d9fd66742777f2d29da37b84fd
Oskar Młynarczuk <Oss106a@gmail.com>
2019-12-01 17:28:27 +0100

    Update OskarMlynarczukcw5.m

--- OskarMlynarczukcw5.m
+++ OskarMlynarczukcw5.m
@@ -1,7 +1,19 @@
-clc
 clear
+clc
 format longg
-F=51.718910115182*pi/180;
-La= 18.6824004701369*pi/180;
+X=3763917.000
+Y=1235727.000
+Z=4982084.000+33*15
+A=6378137.000;
+e2=0.00669438002290;
+WspKart=[X,Y,Z]
+hirvonen(X,Y,Z)
+sAB=35125.123;
+azAB=(70*pi/180)+33*pi/180;
+zAB=(85*pi/180)+33*pi/(180*60);
+F=WspGeo(1)
+La=WspGeo(2)
+Kivioji(sAB,azAB,zAB,F,La)
+
 Wsp1992(F,La)
-Wsp2000(F,La)
+Wsp2000(Fb,Lab)

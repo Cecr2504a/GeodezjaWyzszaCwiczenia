fc615baef9e6b72d12cabf81bce680f0bdc7a98e
Oskar Młynarczuk <Oss106a@gmail.com>
2019-12-01 17:53:16 +0100

    Update OskarMlynarczukcw4.m

--- OskarMlynarczukcw4.m
+++ OskarMlynarczukcw4.m
@@ -4,17 +4,17 @@ format longg
 X=3763917.000
 Y=1235727.000
 Z=4982084.000+33*15
-A=6378137.000;
+a=6378137.000;
 e2=0.00669438002290;
-WspKart=[X,Y,Z]
-hirvonen(X,Y,Z)
+WspKart=[X,Y,Z];
+Hirvonen(X,Y,Z);
 sAB=35125.123;
 azAB=(70*pi/180)+33*pi/180;
 zAB=(85*pi/180)+33*pi/(180*60);
 F=WspGeo(1)
 La=WspGeo(2)
-Kivioji(sAB,azAB,zAB,F,La)
+Kivioji(sAB,azAB,zAB,F,La);
 
-Vincent(F,La,Fb,Lab)
-Aabst=Aab*180/pi
-KatStop(Aabst)
+Vincent(F,La,Fb,Lab);
+Aabst=Aab*180/pi;
+KatStop(Aabst);
